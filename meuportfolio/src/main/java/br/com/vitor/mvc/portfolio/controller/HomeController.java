f3ee54e4b8260aef7918f71ef18a09d1fec98759
Vitor Souza <vitor.souza@absoftware.com.br>
2022-11-17 18:54:39 -0300

    atualizando o portfolio

--- meuportfolio/src/main/java/br/com/vitor/mvc/portfolio/controller/HomeController.java
+++ meuportfolio/src/main/java/br/com/vitor/mvc/portfolio/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/home")
+	@GetMapping(value = "/home")
 	public String home() {
-		return "home.html";
+		return "home";
 	}
 }
